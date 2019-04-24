6c9d0e506d89cd9d7a6f81f8f48d6b15f2951b58
Xuhao-Troy <xuhaowxy@qq.com>
2019-04-24 19:47:28 +0800

    Initial commit

---
+++ src/com/xh/demo/Demo.java
@@ -0,0 +1,18 @@
+package com.xh.demo;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+class Demo{
+    public static void main(String[] args) {
+        int today=20190421;
+        String s=String.valueOf(today);
+//        String s=today+"";
+        System.out.println(s);
+
+
+
+    }
+}
+
+
